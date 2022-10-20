802d2c21ab6ae574c119e8e39ea30f96b64bb9ae
Robinkim93 <kal6529@gmail.com>
2022-10-20 21:34:54 +0900

    [ADD : 제품별 detail page 기능 구현 완료]

--- router/index.js
+++ router/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const usersRouter = require("./usersRouter");
+const detailRouter = require("./detailRouter");
 
 const router = express.Router();
 
 router.use("/users", usersRouter.router);
+router.use("/products", detailRouter.router);
 
 module.exports = router;
